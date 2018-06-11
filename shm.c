8ca1103805745f3a469f2b76c0545ae101507d62
qijieguan <qijieguan7@gmail.com>
2018-06-11 16:36:03 -0700

    Update shm.c

--- shm.c
+++ shm.c
@@ -31,24 +31,90 @@ void shminit() {
 int shm_open(int id, char **pointer) {
 
 //you write this
-//*********** CS 153
-for (i = 0; i< 64; i++) {
-    if (shm_table.shm_pages[i].id == id) {//Exists
-      shm_table.shm_pages[i].refcnt++;
-      break;
-    }
-    else if (i == 63) {//Does not exist
-      //kalloc(id);
-      //memset (...);
-      //shm_table.shm_pages[?] = 1;
-    }
+
+int i = 0; 
+  
+acquire(&(shm_table.lock)); // make sure nothing weird happens with both writing to the table at the same time
+  
+for (i = 0; i < 64; i++)
+{
+  if (shm_table.shm_pages[i].id == id)
+    break;
 }
+ 
+// if i == 64, the id was not found
+// otherwise, the id is at index i
+
+if (i != 64) // id exists // is probably good
+{
   
+  //use mappages to add the mapping between the va and pa
+  mappages(myproc()->pgdir, PGROUNDUP(myproc()->sz), PGSIZE, V2P(shm_table.shm_pages[i].frame), PTE_W|PTE_U); // perm writeable and accessible to user
+ 
+  //pointer that will be returned through the function call
+  *pointer = (char *)PGROUNDUP(myproc()->sz); // I think PGROUNDUP goes here
   
-//**********
+  //increase the reference count
+  shm_table.shm_pages[i].refcnt = shm_table.shm_pages[i].refcnt + 1;
+  
+  //update sz
+  myproc()->sz = PGROUNDUP(myproc()->sz) + PGSIZE;
+}
+else // id doesn't exist already, need to make it
+{
+  //find an empty entry in the table
+  int k = 0;
+  
+  for (k = 0; k < 64; k++)
+  {
+    if (shm_table.shm_pages[k].id == 0)
+      break;
+  }
+  
+  //if k == 64, there is no empty entry in the table, return -1
+  if (k == 64)
+  {
+    release(&(shm_table.lock)); // release the lock before returning
+    return -1;
+  }
+  
+  //otherwise k is the first empty entry in the table
+  //initialize id
+  shm_table.shm_pages[k].id = id;
+  
+  //kalloc page
+  
+  char *mem;
+  
+  mem = kalloc();
+  if(mem == 0)
+  {
+    release(&(shm_table.lock)); // release the lock before returning
+    return -1; // error
+  }
+  memset(mem, 0, PGSIZE);
+  
+  shm_table.shm_pages[k].frame = mem;
+  
+  // map the page 
+  mappages(myproc()->pgdir, PGROUNDUP(myproc()->sz), PGSIZE, V2P(shm_table.shm_pages[k].frame), PTE_W|PTE_U);
+  
+  //set refcnt = 1
+  shm_table.shm_pages[k].refcnt = 1;
+  
+  // do the rest as the other part
+  
+  //pointer that will be returned through the function call
+  *pointer = (char *)PGROUNDUP(myproc()->sz); // I think PGROUNDUP goes here
+  
+  //update sz
+  myproc()->sz = PGROUNDUP(myproc()->sz) + PGSIZE;
+}
 
-
-
+// make sure to return the virtual address through the second parameter
+  
+release(&(shm_table.lock)); // release the lock
+  
 return 0; //added to remove compiler warning -- you should decide what to return
 }
 
@@ -57,7 +123,10 @@ int shm_close(int id) {
 //you write this too!
 
 // look for the id passed in
-int i = 0;
+int i = 0; 
+  
+acquire(&(shm_table.lock)); // make sure nothing weird happens with both writing to the table at the same time
+  
 for (i = 0; i < 64; i++)
 {
   if (shm_table.shm_pages[i].id == id)
@@ -73,6 +142,7 @@ if (i != 64 && shm_table.shm_pages[i].refcnt != 0)
 }
 else
 {
+  release(&(shm_table.lock)); // release in case of error
   return -1; // error
 }
 
@@ -82,6 +152,8 @@ if (shm_table.shm_pages[i].refcnt == 0) // clear the table entry
   shm_table.shm_pages[i].frame = 0;
   shm_table.shm_pages[i].refcnt = 0;
 }
+  
+release(&(shm_table.lock)); // now that everything is taken care of, someone else can look at this table
 
 return 0; //added to remove compiler warning -- you should decide what to return
 }
